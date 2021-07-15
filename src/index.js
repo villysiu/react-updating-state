3ffb3ebdd304941589248b9570d6c86a502585e0
villysiu <villysiu@gmail.com>
2021-07-15 14:26:56 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ButtonCounter from './components/ButtonCounter';
+import ClickityClick from './components/ClickityClick';
+import LightSwitch from './components/LightSwitch';
+
+//ReactDOM.render(<ClickityClick />, document.getElementById('root')); 
+
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+<div>
+  <ClickityClick />
+<ButtonCounter />
+<LightSwitch />
+</div>, document.getElementById('root')); 
